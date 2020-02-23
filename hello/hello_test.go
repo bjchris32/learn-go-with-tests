a5912a448de9f0d9808cf7bee76ac3566977220a
Chris Shao <bjchris32@gmail.com>
2020-02-23 23:17:51 +0800

    test(hello): add subtest to greet to the world

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,10 +4,22 @@ import "testing"
 
 // *testing.T is the hook into the testing framework
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	expected := "Hello, Chris"
+	t.Run("greeting to people", func(t *testing.T) {
+		got := Hello("Chris")
+		expected := "Hello, Chris"
 
-	if got != expected {
-		t.Errorf("got %q expected %q", got, expected)
-	}
+		if got != expected {
+			t.Errorf("got %q expected %q", got, expected)
+		}
+	})
+
+
+	t.Run("greeting the world when there is no argument", func(t *testing.T) {
+		got := Hello("")
+		expected := "Hello, World"
+
+		if got != expected {
+			t.Errorf("got %q expected %q", got, expected)
+		}
+	})
 }
