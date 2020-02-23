ec8dc38b4ab7eb581df35c135f144e846364185d
Chris Shao <bjchris32@gmail.com>
2020-02-23 23:26:25 +0800

    test(hello): extrac the assertion to a function

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,22 +4,23 @@ import "testing"
 
 // *testing.T is the hook into the testing framework
 func TestHello(t *testing.T) {
-	t.Run("greeting to people", func(t *testing.T) {
-		got := Hello("Chris")
-		expected := "Hello, Chris"
-
+	assertMessage := func(t *testing.T, got string, expected string) {
+		t.Helper() // for tracing the failure line number in the caller of this assertion
 		if got != expected {
 			t.Errorf("got %q expected %q", got, expected)
 		}
+	}
+
+	t.Run("greeting to people", func(t *testing.T) {
+		got := Hello("Chris")
+		expected := "Hello, Chris"
+		assertMessage(t, got, expected)
 	})
 
 
 	t.Run("greeting the world when there is no argument", func(t *testing.T) {
 		got := Hello("")
 		expected := "Hello, World"
-
-		if got != expected {
-			t.Errorf("got %q expected %q", got, expected)
-		}
+		assertMessage(t, got, expected)
 	})
 }
