8692ae2d0858518ebbdbc187242cee7f3a5f8a18
Chris Shao <bjchris32@gmail.com>
2020-02-23 23:11:13 +0800

    feat(hello): use const and greeting to someone

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
-func Hello() string {
-    return "Hello, world"
+const helloPrefix = "Hello, "
+
+func Hello(name string) string {
+    return helloPrefix + name
 }
 
 func main() {
-    fmt.Println(Hello())
+    fmt.Println(Hello("world"))
 }
 
