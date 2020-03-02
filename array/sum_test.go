bcca604bd855bdd5f524a0d70e1efe7ecf01e471
Chris Shao <bjchris32@gmail.com>
2020-03-02 22:52:10 +0800

    test(slice): add test cases for sum of tails

--- array/sum_test.go
+++ array/sum_test.go
@@ -24,3 +24,24 @@ func TestSumAll(t *testing.T) {
 		t.Errorf("got %v expected %v", got, expected)
 	}
 }
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		expected := []int{2, 9}
+
+		if !reflect.DeepEqual(got, expected) {
+			t.Errorf("got %v expected %v", got, expected)
+		}
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 9})
+		expected := []int{0, 13}
+
+		if !reflect.DeepEqual(got, expected) {
+			t.Errorf("got %v expected %v", got, expected)
+		}
+	})
+}
+
