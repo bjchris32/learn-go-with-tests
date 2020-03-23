67f7d812ab9de8c576c5ed0c0fa6b51d273ee87c
Chris Shao <bjchris32@gmail.com>
2020-03-23 21:54:37 +0800

    test(shapes): extract the commom part

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -13,23 +13,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle { 12, 6 }
-		got := rectangle.Area()
-		expected := 72.0
-
+	checkArea := func(t *testing.T, shape Shape, expected float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != expected {
-			t.Errorf("get %g expected %g", got, expected)
+			t.Errorf("got %g expected %g", got, expected)
 		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12, 6}
+		checkArea(t, rectangle, 72.0)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle { 10 }
-		got := circle.Area()
-		expected := 314.1592653589793
-
-		if got != expected {
-			t.Errorf("get %g expected %g", got, expected)
-		}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
