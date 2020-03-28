a5320d2043291a7931d32c30bfbaab5459aeda8b
Chris Shao <bjchris32@gmail.com>
2020-03-28 23:13:32 +0800

    test(wallet): extract the error assertion function

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -12,6 +12,13 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
+	assertError := func(t *testing.T, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("expected an error but it did not get one")
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -32,8 +39,6 @@ func TestWallet(t *testing.T) {
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertBalance(t, wallet, startingBalance)
-		if err == nil {
-			t.Error("expected an error but didn't get one")
-		}
+		assertError(t, err)
 	})
 }
