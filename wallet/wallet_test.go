8a24c2e803ed20a357b54073cab2ba50d5847954
Chris Shao <bjchris32@gmail.com>
2020-03-28 23:07:02 +0800

    test(wallet): add the test case to handle insufficient balance error

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -12,8 +12,6 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
-
-
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -26,6 +24,17 @@ func TestWallet(t *testing.T) {
 		wallet.Withdraw(Bitcoin(10))
 
 		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
 	})
 
 
