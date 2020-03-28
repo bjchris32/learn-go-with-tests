154c700e60e0c4d9d8a7d169567e8136dc3b6491
Chris Shao <bjchris32@gmail.com>
2020-03-28 22:52:43 +0800

    test(wallet): add the test case for withdraw function

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -3,13 +3,29 @@ package wallet
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance()
-	expected := Bitcoin(10)
+		got := wallet.Balance()
+		expected := Bitcoin(10)
 
-	if got != expected {
-		t.Errorf("got %s expected %s", got, expected)
-	}
+		if got != expected {
+			t.Errorf("got %s expected %s", got, expected)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+		expected := Bitcoin(10)
+
+		if got != expected {
+			t.Errorf("got %s expected %s", got, expected)
+		}
+	})
+
+
 }
