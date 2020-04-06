ba3de0380f931eba0f9d10989e88b32e690ff127
Chris Shao <bjchris32@gmail.com>
2020-04-06 23:51:48 +0800

    test(dictionary): add the test case for adding a word into dictionary

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -19,6 +19,21 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	expected := "this is just a test"
+	got, err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if got != expected {
+		t.Errorf("got %q expected %q", got, expected)
+	}
+}
+
 func assertStrings(t *testing.T, got, expected string) {
 	t.Helper()
 
