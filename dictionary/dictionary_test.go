23eb7e7c4395c244d71b998cc00f71656ce3ebd4
Chris Shao <bjchris32@gmail.com>
2020-04-06 23:35:40 +0800

    test(dictionary): add test cases for unknown word in the dictionary

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -5,10 +5,23 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary { "test": "this is just a test" }
 
-	got := Search(dictionary, "test")
-	expected := "this is just a test"
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		expected := "this is just a test"
 
-	assertStrings(t, got, expected)
+		assertStrings(t, got, expected)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		expected := "could ont find the word"
+
+		if err == nil {
+			t.Fatal("expected to get an error")
+		}
+
+		assertStrings(t, err.Error(), expected)
+	})
 }
 
 func assertStrings(t *testing.T, got, expected string) {
