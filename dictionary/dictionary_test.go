08b99cc15d2280d713a22939e677c61ee5301634
Chris Shao <bjchris32@gmail.com>
2020-04-07 23:27:33 +0800

    test(dictionary): add the test case for adding existing word definition

--- dictionary/dictionary_test.go
+++ dictionary/dictionary_test.go
@@ -20,12 +20,25 @@ func TestSearch(t *testing.T) {
 }
 
 func TestAdd(t *testing.T) {
-	dictionary := Dictionary{}
-	word := "test"
-	definition := "this is just a test"
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		definition := "this is just a test"
 
-	dictionary.Add(word, definition)
-	assertDefinition(t, dictionary, word, definition)
+		err := dictionary.Add(word, definition)
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{ word: definition }
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
 }
 
 func assertStrings(t *testing.T, got, expected string) {
@@ -41,6 +54,13 @@ func assertError(t *testing.T, got, expected error) {
 
 	if got != expected {
 		t.Errorf("got error %q expected %q", got, expected)
+	}
+
+	if got == nil {
+		if expected == nil {
+			return
+		}
+		t.Fatal("expected to get an error.")
 	}
 }
 
