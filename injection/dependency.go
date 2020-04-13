aba7e2f3c40ca30b1a07c031843f3f85e4179556
Chris Shao <bjchris32@gmail.com>
2020-04-13 23:21:46 +0800

    feat(dependency): use io.Writer, so that it would be compatible in both application and test

--- injection/dependency.go
+++ injection/dependency.go
@@ -1,10 +1,19 @@
 package main
 
-import "bytes"
-import "fmt"
+import (
+    "fmt"
+    "os"
+    "io"
+)
 
 
-func Greet(writer *bytes.Buffer, name string) {
+// both bytes.Buffer and os.Stdout implement io.Writer
+// So, io.Writer could be used both in test and application
+func Greet(writer io.Writer, name string) {
 	// fmt.Printf("Hello, %s", name) // send the string into the default stdout
 	fmt.Fprintf(writer, "Hello, %s", name) // send the string into the buffer
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
 }
