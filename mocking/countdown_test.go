a269dede4a80e9f3780635990d5e76136106e37e
Chris Shao <bjchris32@gmail.com>
2020-04-16 23:53:11 +0800

    test(count down): use another spy to assert the order

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,6 +2,7 @@ package main
 
 import "testing"
 import "bytes"
+import "reflect"
 
 
 func TestCountdown(t *testing.T) {
@@ -23,4 +24,24 @@ Go!`
 	if spySleeper.Calls != 4 {
 		t.Errorf("not enough calls to sleeper, expected 4 got %d", spySleeper.Calls)
 	}
+
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &CountdownOperationsSpy {}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		expected := []string{
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(expected, spySleepPrinter.Calls) {
+			t.Errorf("expected calls %v got %v", expected, spySleepPrinter.Calls)
+		}
+	})
 }
