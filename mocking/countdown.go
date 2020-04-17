6a52c3e9774a0975f74a4c647001c8a262fa5d41
Chris Shao <bjchris32@gmail.com>
2020-04-17 23:55:46 +0800

    feat(count down): remove the unused spy

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -17,14 +17,6 @@ type Sleeper interface {
 	Sleep()
 }
 
-type SpySleeper struct {
-	Calls int
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
-
 type CountdownOperationsSpy struct {
 	Calls []string
 }
