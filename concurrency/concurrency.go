682c753440ac1e36a5d0f5ae474f2182fdf2130c
Chris Shao <bjchris32@gmail.com>
2020-04-21 23:17:47 +0800

    feat(concurrency): use channel to achieve concurrency

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,21 +1,28 @@
 package concurrency
 
-import "time"
-
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultChannel := make(chan result)
 
 	for _, url := range urls {
 		// we need the argument u, so that the goroutine would not share the same url variable
 		go func(u string) {
-			results[u] = wc(u)
+			// whenever it finishes, it puts the result into the channel
+			resultChannel <- result{u , wc(u)}
 		}(url)
     }
 
-    // wait for the goroutines done
-    time.Sleep(2 * time.Second)
+	for i := 0; i < len(urls); i++ {
+		// read the result from channel
+		result := <-resultChannel
+		results[result.string] = result.bool
+	}
 
-    return results
+	return results
 }
