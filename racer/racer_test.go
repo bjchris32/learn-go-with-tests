dbe66f584bb4b966f11be84e87c91a51920fb06c
Chris Shao <bjchris32@gmail.com>
2020-04-23 23:41:30 +0800

    test(racer): use httptest

--- racer/racer_test.go
+++ racer/racer_test.go
@@ -1,10 +1,24 @@
 package racer
 
-import "testing"
+import(
+	"testing"
+	"net/http/httptest"
+	"time"
+	"net/http"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.co.uk"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	expect := fastURL
 	got := Racer(slowURL, fastURL)
@@ -12,4 +26,7 @@ func TestRacer(t *testing.T) {
 	if got != expect {
 		t.Errorf("got %q, expect %q", got, expect)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
