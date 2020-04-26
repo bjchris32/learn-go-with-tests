0ac7b03b360923bd36681cb090bccac2f09533a4
Chris Shao <bjchris32@gmail.com>
2020-04-26 23:26:33 +0800

    feat(racer): use the configurable racer

--- racer/racer.go
+++ racer/racer.go
@@ -6,14 +6,21 @@ import (
 	"net/http"
 )
 
+var tenSecondTimeout = 10 * time.Second
+
 func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
+}
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
 	// wait multiple channel at the same time
 	select {
 	case <-ping(a):
 		return a, nil
 	case <-ping(b):
 		return b, nil
-	case <-time.After(10 * time.Second):
+	// this case prevents the program from blocking forever
+	case <-time.After(timeout):
 		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
