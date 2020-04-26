567cc80f91ed85f90bc897a8cd266b61e7688b63
Chris Shao <bjchris32@gmail.com>
2020-04-26 23:09:44 +0800

    test(racer): add test case for raising error within 10s

--- racer/racer_test.go
+++ racer/racer_test.go
@@ -18,11 +18,25 @@ func TestRacer(t *testing.T) {
 	fastURL := fastServer.URL
 
 	expect := fastURL
-	got := Racer(slowURL, fastURL)
+	got, _ := Racer(slowURL, fastURL)
 
 	if got != expect {
 		t.Errorf("got %q, expect %q", got, expect)
 	}
+
+	t.Run("returns an error if a server doesn't respond withing 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Second)
+		serverB := makeDelayedServer(12 * time.Second)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL)
+
+		if err == nil {
+			t.Errorf("expected an error but did not get one")
+		}
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
