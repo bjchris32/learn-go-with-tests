055f2bba45b1bb74f4880ab5979ae47e44073dba
Chris Shao <bjchris32@gmail.com>
2020-08-23 15:19:55 +0800

    test(sync): add a failed test case without using mutex
    concurrent goroutine cannot modify the same value at the same time, even using sync package

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,6 +1,8 @@
 package main
 
 import "testing"
+import "sync"
+
 
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
@@ -12,6 +14,24 @@ func TestCounter(t *testing.T) {
 		assertCounter(t, counter, 3)
 	})
 
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		expectedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(expectedCount)
+
+		for i := 0; i < expectedCount; i++ {
+			go func(w *sync.WaitGroup) {
+				counter.Inc()
+				w.Done()
+			}(&wg)
+		}
+
+		wg.Wait()
+
+		assertCounter(t, counter, expectedCount)
+	})
 }
 
 func assertCounter(t *testing.T, got Counter, expected int) {
