c5466a8eae64ff6db75e6e47c65388a6d2d8308a
Chris Shao <bjchris32@gmail.com>
2020-10-18 17:07:17 +0800

    test(sync): add some comments

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 import "sync"
 
-
+// communicate by sharing by memory
 func TestCounter(t *testing.T) {
 	t.Run("incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
 		counter := Counter{}
