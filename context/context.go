77e3903d9dacfc2dd3b629512fee3f2ed301521c
Chris Shao <bjchris32@gmail.com>
2020-10-21 08:54:08 +0800

    test(context): add the test case for http handler

---
+++ context/context.go
@@ -0,0 +1,14 @@
+package main
+
+import "net/http"
+import "fmt"
+
+type Store interface {
+	Fetch() string
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, store.Fetch())
+	}
+}
