21847f65ded3865dc158d89b973fe8bc4e31d381
Chris Shao <bjchris32@gmail.com>
2024-05-31 15:19:01 -0500

    feat(iterations): allow users to pass in the number of iterations

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,9 +1,16 @@
 package iteration
 
-// repeat the string for 5 times
-func Repeat(character string) string {
+// repeat the string for given times. If no iterations given or the given iterations is less than 1, repeat just 1 time
+func Repeat(character string, iterations ...int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	num_iterations := 1
+	if len(iterations) > 0 {
+		if iterations[0] > 0 {
+			num_iterations = iterations[0]
+		}
+	}
+
+	for i := 0; i < num_iterations; i++ {
 		repeated += character
 	}
 	return repeated
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,25 +1,45 @@
 package iteration
 
-import "testing"
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
-
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+	assertMessage := func(t *testing.T, got string, expected string) {
+		t.Helper() // for tracing the failure line number in the caller of this assertion
+		if got != expected {
+			t.Errorf("got %q expected %q", got, expected)
+		}
 	}
+
+	t.Run("repeat the string for 8 times when number of iterations is given", func(t *testing.T) {
+		got := Repeat("a", 8)
+		expected := "aaaaaaaa"
+		assertMessage(t, got, expected)
+	})
+
+	t.Run("repeat the string for 1 times when there is no iteration parameter given", func(t *testing.T) {
+		got := Repeat("a")
+		expected := "a"
+		assertMessage(t, got, expected)
+	})
 }
 
-func ExampleRepeat() {
-	repeated := Repeat("ab")
+func ExampleRepeat_1() {
+	repeated := Repeat("ab", 2)
 	fmt.Println(repeated)
-	// Output: ababababab
+	// Output: abab
+}
+
+func ExampleRepeat_2() {
+	repeated := Repeat("ab", -1)
+	fmt.Println(repeated)
+	// Output: ab
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 2)
 	}
 }
