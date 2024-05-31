c14726f41b5f22fb8e9c540a86c7de9df3796b45
Chris Shao <bjchris32@gmail.com>
2024-05-31 15:51:35 -0500

    doc: add referring documents

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,6 +1,7 @@
 package iteration
 
 // repeat the string for given times. If no iterations given or the given iterations is less than 1, repeat just 1 time
+// use one of the methods to pass optional argument refering to https://joneisen.me/programming/2013/06/23/golang-and-default-values.html
 func Repeat(character string, iterations ...int) string {
 	var repeated string
 	num_iterations := 1
